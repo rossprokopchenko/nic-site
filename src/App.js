b149a7d9999e1645c3db7dd6a6602f57f395ee28
Ross Prokopchenko <rosskakashka@gmail.com>
2022-09-14 11:13:16 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+import { Typography, Box } from '@mui/material';
+import Bubs from './resources/BUBS.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className="App" justifyContent='center' alignItems='center' sx={{height: '100vh', border: '1px solid red'}}>
+      <Typography variant='h4'>
+        <strong>ERROR 69</strong> - Bubbs could not find the website you're looking for
+      </Typography>
+
+      <img src={Bubs} style={{height: '500px'}} />
+    </Box>
   );
 }
 
